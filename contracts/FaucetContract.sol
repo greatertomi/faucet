8185cd113b55cfe95e2e41e2b53860e07f3527da
John Oshalusi <john.oshalusi@iohk.io>
2022-07-04 22:45:58 +0100

    get funders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,10 +2,19 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-
+  address[] public funders;
   // This is called when you make a transaction that doesn't specify function to call.
   // External functions are part of the public interface of the contract.
   // They can be called by via contracts and other transactions.
   receive() external payable {}
 
+  function addFunds() external payable {
+    funders.push(msg.sender);
+  }
+
+  function getAllFunders() public view returns (address[] memory) {
+    return funders;
+  }
 }
+
+// const instance = await Faucet.deployed()
