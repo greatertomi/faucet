5e5a9a77bde720ac8ecc29990fd3875342011068
John Oshalusi <john.oshalusi@iohk.io>
2022-07-09 00:22:22 +0100

    Improve getting funders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,19 +2,37 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-  address[] public funders;
+  uint public numOfFunders;
+  mapping(address => bool) private funders;
+  mapping(uint => address) private lutFunders;
+
   // This is called when you make a transaction that doesn't specify function to call.
   // External functions are part of the public interface of the contract.
   // They can be called by via contracts and other transactions.
   receive() external payable {}
 
   function addFunds() external payable {
-    funders.push(msg.sender);
+    address funder = msg.sender;
+
+    if (!funders[funder]) {
+      uint index = numOfFunders++;
+      funders[funder] = true;
+      lutFunders[index] = funder;
+    }
   }
 
   function getAllFunders() public view returns (address[] memory) {
-    return funders;
+    address[] memory _funders = new address[](numOfFunders);
+    for (uint i = 0; i < numOfFunders; i++) {
+      _funders[i] = lutFunders[i];
+    }
+    return _funders;
+  }
+
+  function getFunderAtIndex(uint8 index) external view returns(address) {
+    return lutFunders[index];
   }
 }
 
 // const instance = await Faucet.deployed()
+// instance.addFunds({from: accounts[0], value: "2000000000000000000"})
