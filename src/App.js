b0a634d817aabe0796a9a556f1f640d23a770da5
John Oshalusi <john.oshalusi@iohk.io>
2022-07-11 22:43:21 +0100

    Work on adding provider to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import "./App.css";
+import Web3 from "web3";
 
 const App = () => {
+  const [web3Api, setWeb3Api] = useState({provider: null, web3: null});
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      // With metamask we have access to window.ethereum & to window.web3
+      // metamarsk injects a global API into website. This API will allow websites
+      // to request users, accounts, read to blockchain, sign messages and transactions.
+      let provider = null;
+      if (window.ethereum) {
+        provider = window.ethereum;
+        try {
+          await provider.request({method: "eth_requestAccounts"});
+        } catch (err) {
+          console.error("User denied account access");
+        }
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider
+      })
+    }
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    }
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
+
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <span>
+            <strong>Account:</strong>
+          </span>
+          <h1>
+            {account ? account : "not connected"}
+          </h1>
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
